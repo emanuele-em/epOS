edcb689e87790563c1db9a5425940bf32aa73a04
emanuele-em <micheletti.emanuele@hotmail.com>
2023-02-22 17:02:48 +0100

    update files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-#![feature(panic_handler)]
-#![feature(core_intrinsics)]
 #![no_std]
 #![no_main]
 
-extern crate bootloader;
-
-use core::instrinsics;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-#[no_mangle]
 fn panic(_info: &PanicInfo) -> ! {
-    unsafe{ instrinsics::abort() }
+    loop {}
 }
 
+static HELLO: &[u8] = b"ep_os: Ciao Elenuccia, stai tranquilla per l'esame ok?";
+
 #[no_mangle]
-pub fn _start() -> ! {
-    loop{}
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8; //row pointer to first VGA buffer cell
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
